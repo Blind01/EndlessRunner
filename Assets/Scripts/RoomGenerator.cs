14e5779379e331cde35be75ba500257305aecc66
Dustey <11064124@brookes.ac.uk>
2015-04-23 15:13:45 +0100

    Pause menu
    New pause menu, bug fixes etc.

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -14,6 +14,7 @@ public class RoomGenerator : MonoBehaviour {
 	public Text distanceText;
 	public Text barrierText;
 	public Text roomText;
+	public Canvas pauseMenu;
 
 	private GameObject previousRoom;
 	private GameObject currentRoom;
@@ -27,6 +28,7 @@ public class RoomGenerator : MonoBehaviour {
 	float multiplier = 1.0f;
 	int pickupCounter = 0;
 	int roomCounter = 1;
+	bool paused = false;
 	
 
 	// Use this for initialization
@@ -54,12 +56,18 @@ public class RoomGenerator : MonoBehaviour {
 	void Start(){
 
 		killBarrierScript = killBarrier.gameObject.GetComponent<KillBarrierScript> ();
+		pauseMenu.enabled = false;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player != null) {
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			if(paused) unpause();
+			else pause ();
+		}
+
+		if (player != null && Time.timeScale != 0) {
 			pDistance = (int)Vector3.Distance (transform.position, player.transform.position);
 			bDistance = (int)Vector3.Distance (player.transform.position, killBarrier.transform.position);
 
@@ -101,4 +109,28 @@ public class RoomGenerator : MonoBehaviour {
 		Application.LoadLevel ("SummaryScreen");
 	
 	}
+
+	 void pause(){
+		paused = true;
+		Time.timeScale = 0;
+		pauseMenu.enabled = true;
+
+	}
+
+	public void unpause(){
+		paused = false;
+		Time.timeScale = 1;
+		pauseMenu.enabled = false;
+	}
+
+	public void restartGame(){
+		unpause ();
+		Application.LoadLevel("mainScene");
+	}
+
+	public void quitGame(){
+		unpause ();
+		Application.LoadLevel("TitleScreen");
+	}
+
 }
